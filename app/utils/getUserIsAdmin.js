524aabf8186edb4d65cae3b4ec12b484ba4d5c03
lns <bezborodnikovtimur@gmail.com>
2024-10-16 00:29:17 +0300

    Update getUserIsAdmin.js

--- app/utils/getUserIsAdmin.js
+++ app/utils/getUserIsAdmin.js
@@ -1,5 +1,5 @@
 export function getUserIsAdmin(token) {
-  if (token !== "0" & token !== null) {
+  if (token !== "0" && token !== null) {
     const parts = token.split(".");
     const payload = JSON.parse(atob(parts[1])); // полезная информация
     const userRoles = payload.roles; // роли пользователя

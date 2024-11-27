0c428f84f813c4a957c92623916bbe3764b9ea19
lns <142323634+lnnsss@users.noreply.github.com>
2024-11-27 10:09:03 +0300

    Add files via upload

--- backend/handleError.js
+++ backend/handleError.js
@@ -1,12 +1,6 @@
-// Функция для вывода ошибок
-const handleError = (res, err) => {
-  if (err.name === "ValidationError") {
-    return res
-      .status(400)
-      .json({ message: "Ошибка валидации", errors: err.errors });
-  }
-
-  return res.status(500).json({ err });
-};
-
-module.exports = handleError;
+export const handleError = (err) => {
+    console.error(err);
+    res.status(500).json({
+        message: err.message,
+    });
+}
